df360a8871a6b4ef4b837b191e45135d3a6538d2
James South <james_south@hotmail.com>
2013-11-13 18:22:33 +0000

    v2.0.1

--- rel/responsive.js
+++ rel/responsive.js
@@ -19,7 +19,7 @@
     Licensed under the Apache License v2.0.
     ============================================================================== */
 
-/*! Responsive v2.0.0 | Apache v2.0 License | git.io/rRNRLA */
+/*! Responsive v2.0.1 | Apache v2.0 License | git.io/rRNRLA */
 
 /*
  * Responsive Utils
