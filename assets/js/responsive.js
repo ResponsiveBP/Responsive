ae9689dce1824a7f8640033b6f508f9eed9ac91c
James Jackson-South <james_south@hotmail.com>
2017-03-29 14:45:55 +1100

    Update assets

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -6,7 +6,7 @@
     Licensed under the MIT License.
     ============================================================================== */
 
-/*! Responsive v4.1.3 | MIT License | responsivebp.com */
+/*! Responsive v4.1.4 | MIT License | responsivebp.com */
 
 /*
  * Responsive Core
@@ -3015,19 +3015,19 @@
                 self.$element.trigger($.Event(eadded));
             };
 
-        this.$element.trigger(addEvent);
+        self.$element.trigger(addEvent);
 
         if (addEvent.isDefaultPrevented()) {
 
             return;
         }
 
-        this.isAdded = true;
+        self.isAdded = true;
 
-        $.each(this.$bodyRows, function () {
+        $.each(self.$bodyRows, function () {
 
             var selector = self.hasHeader ? "th, td" : "td";
-
+    
             $(this).find(selector).each(function (index) {
 
                 var $this = $(this),
@@ -3048,7 +3048,7 @@
                     $this.attr({ "aria-role": "gridcell", "aria-describedby": footerId });
                 }
 
-                return false;
+                return true;
             });
         });
 
