52c5ba37cd2b4753fe0e85037a9381dda1f5571e
James Jackson-South <james_south@hotmail.com>
2017-03-29 14:16:46 +1100

    v4.1.4

--- src/js/responsive.core.js
+++ src/js/responsive.core.js
@@ -6,7 +6,7 @@
     Licensed under the MIT License.
     ============================================================================== */
 
-/*! Responsive v4.1.3 | MIT License | responsivebp.com */
+/*! Responsive v4.1.4 | MIT License | responsivebp.com */
 
 /*
  * Responsive Core
